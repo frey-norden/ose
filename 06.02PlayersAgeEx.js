1c46cf9d9eafbaaf0b0fef2ddd79337100735d00
frey-norden <frey.norden@icloud.com>
2020-09-22 23:43:50 +0300

    two more conditional scripts

---
+++ 06.02PlayersAgeEx.js
@@ -0,0 +1,20 @@
+function checkAge() {
+		// assign the age input field value into a variable
+    var age =
+        Number(document.getElementById('age').value);
+		//if age smaller than 18
+    if (age < 18) {
+      // assign text "Adolescents are not allowed to play." to a variable
+      var status = "Adolescents are not allowed to play.";
+    } else if (age >= 18) {
+      // otherwise
+      var status = "Old enough to play.";
+			// assign text "Old enough to play." to a variable
+
+		}
+
+		// Write the answer to the web page DOM, in the answer div, as the content
+    document.getElementById('answer').innerHTML =
+      status;
+
+}
