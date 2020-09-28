3b2009572673891a9603964f7c828698e728212f
frey-norden <frey.norden@icloud.com>
2020-09-28 09:46:38 +0300

    added + to loop

--- 08.01SimpleForLoopEx.js
+++ 08.01SimpleForLoopEx.js
@@ -2,7 +2,7 @@ function runForLoop() {
   var divOut = document.getElementById('divOut');
   var i;
   for (i = 0; i < 31; i++) {
-    divOut.innerHTML = i + "<br>";
+    divOut.innerHTML += i + "<br>";
     console.log(i);
   }
 
