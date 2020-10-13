269850bef275d6bdb77d50317c685d2eb3005194
frey-norden <frey.norden@icloud.com>
2020-10-14 00:12:33 +0300

    kela reimbursement calculator

--- 07.06aDoctorVisitAndKelaEx.js
+++ 07.06aDoctorVisitAndKelaEx.js
@@ -2,18 +2,26 @@
 function kelaReimbursement(visitLength) {
   if (visitLength < 10) {
     return 8.00;
-  }
-  else if (visitLength >= 10 && visitLength < 20) {
+  } else if (visitLength >= 10 && visitLength < 20) {
     return 11.00;
+  } else if (visitLength >= 20 && visitLength < 30) {
+    return 13.50;
+  } else if (visitLength >= 30 && visitLength < 45) {
+    return 16.50;
+  } else {
+    return 21.00;
   }
 }
 // Define a function
 function calculate() {
 		// Read values from the input fields
-
+    var visitLength = Number(document.getElementById('length').value);
+    var doctorsFee = Number(document.getElementById('doctorsFee').value);
 		// Call the function (1.) which calculates and returns Kela reimbursement
-
+    var reimbursement = kelaReimbursement(visitLength);
 
 		// Write the answer on the page, to the answer div, as content of the div
-
+    var outputVisit = 'Length of visit is ' + visitLength + ' minutes. <br>';
+    var outputRepay = 'Kela reimbursement is ' + reimbursement.toFixed(2) + ' euros.';
+    document.getElementById('answer').innerHTML = outputVisit + outputRepay;
 }
