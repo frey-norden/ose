def332a9f933176d66c2f768704272723a18de78
frey-norden <frey.norden@icloud.com>
2020-10-27 23:20:54 +0200

    doctor visit

--- 07.06aDoctorVisitAndKelaEx.js
+++ 07.06aDoctorVisitAndKelaEx.js
@@ -12,6 +12,13 @@ function kelaReimbursement(visitLength) {
     return 21.00;
   }
 }
+// function to calculate how much owed after kela reimbursement
+function amountDue(doctorsFee, reimbursement) {
+  var officeFee = 15.90;
+  var toPay = doctorsFee + officeFee - reimbursement;
+  return toPay;
+}
+
 // Define a function
 function calculate() {
 		// Read values from the input fields
@@ -19,9 +26,13 @@ function calculate() {
     var doctorsFee = Number(document.getElementById('doctorsFee').value);
 		// Call the function (1.) which calculates and returns Kela reimbursement
     var reimbursement = kelaReimbursement(visitLength);
+    var paymentDue = amountDue(doctorsFee, reimbursement);
 
 		// Write the answer on the page, to the answer div, as content of the div
-    var outputVisit = 'Length of visit is ' + visitLength + ' minutes. <br>';
-    var outputRepay = 'Kela reimbursement is ' + reimbursement.toFixed(2) + ' euros.';
-    document.getElementById('answer').innerHTML = outputVisit + outputRepay;
+    var outputVisit = "Doctor's Fee is " + doctorsFee.toFixed(2) + ' euros.<br>';
+    var outputRepay = 'Kela reimbursement is ' + reimbursement.toFixed(2) + ' euros.<br>';
+    var officeFee = 'Office Fee is 15.90 euros<br>';
+    var need2Pay = 'Customer needs to pay ' + paymentDue.toFixed(2) + ' euros.'
+
+    document.getElementById('answer').innerHTML = outputVisit + outputRepay + officeFee + need2Pay;
 }
