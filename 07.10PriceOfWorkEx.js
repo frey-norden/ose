158a422f96563ae532e82aca2b46b49dd5721ba5
frey-norden <frey.norden@icloud.com>
2020-11-02 23:54:45 +0200

    calculation works!!

--- 07.10PriceOfWorkEx.js
+++ 07.10PriceOfWorkEx.js
@@ -34,14 +34,14 @@ function calculate() {
     // startTime field
     var sTHours = startTime.substr(0, 2);
     sTHours = parseInt(sTHours) * 60;
-    var sTMins = startTime.substr(3, 5);
+    var sTMins = startTime.substr(3, 2);
     sTMins = parseInt(sTMins);
     var sT = sTHours + sTMins;
 
     // endTime field
     var eTHours = endTime.substr(0, 2);
     eTHours = parseInt(eTHours) * 60;
-    var eTMins = endTime.substr(3, 5);
+    var eTMins = endTime.substr(3, 2);
     eTMins = parseInt(eTMins);
     var eT = eTHours + eTMins;
     // actual calculation
@@ -49,18 +49,24 @@ function calculate() {
 
 
     var dateInput = document.getElementById('date').value;
-    var checkDay = isSunday(dateInput);
+    var issunday = isSunday(dateInput);
+
     // If it is not Sunday  (Call isSunday() function)
         //Calculate and show price of the repair work during the workdays
     //otherwise
         //Calculate and showprice of the repair work on Sundays
+    var totalPrice = (totalTime / 60);
 
+    if (issunday == true) {
+       var rate = 72; // labor rate for Sundays
 
-    var totalPrice = (totalTime / 60) * 48;
-
+    } else {
+      var rate = 48;  // labor rate for all other days
+    }
+    totalPrice *= rate;
 
     var outputMsg1 = 'Length of the work was ' + totalTime + ' minutes.<br>'
-    var outputMsg2 = 'The hourly price is during the workdays 48.00 euros.<br>'
+    var outputMsg2 = 'The hourly price is during the workdays ' + rate.toFixed(2) +' euros.<br>'
     var outputMsg3 = 'The price of this repair work is ' + totalPrice.toFixed(2) + ' euros.'
     document.getElementById('answer').innerHTML = outputMsg1 + outputMsg2 + outputMsg3;
 }
@@ -76,17 +82,14 @@ function isSunday(dateText) {
   var yr = dateText.substr(6, 4);
 
 	// Create a new Date object to a variable asDate
-  var workDate = new Date();
+  var workDate = new Date(yr, mon - 1, day);
+  // Get the day of the week
+  var whatDay = workDate.getDay();
 
-	// Get the day of the week with its getDay() method. Sunday is number 0 .
-
-
-	// If day of the week is Sunday
-
-		// return Boolean value true
-
-	// otherwise
-
-       // return Boolean value false
+	if (whatDay == 0) {
+    return true;
+  } else {
+    return false;
+  }
 
 }
